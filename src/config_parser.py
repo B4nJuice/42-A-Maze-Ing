7533ccbbf1006aadc40649a1b514485a004395b6
Girard Louis <lgirard@student.42lyon.fr>
2026-01-19 12:52:52 +0100

    linted config

--- src/config_parser.py
+++ src/config_parser.py
@@ -29,7 +29,8 @@ class Config():
         for line in get_next_line(file):
             parameter, value = get_value(line)
             if parameter in parameters:
-                new_value = self.apply_types(parameter, config[parameter], value)
+                new_value = self.apply_types(parameter, config[parameter],
+                                             value)
                 config[parameter][0] = new_value
                 config[parameter][2] = True
                 # self.fill_param(config, parameter, value)
@@ -60,19 +61,22 @@ class Config():
             elif value.capitalize() == "False":
                 value = False
             else:
-                raise (ConfigError(f"invalid argument\"{value}\" for {parameter}"))
+                raise (ConfigError(f"invalid argument\"{value}\"\
+ for {parameter}"))
         elif real_type[0] == tuple:
             separator = real_type[3]
             n_types = real_type[1]
             new_value = value.split(separator)
 
             if n_types != len(new_value):
-                raise (ConfigError(f"invalid argument\"{value}\" for {parameter}"))
+                raise (ConfigError(f"invalid argument\"{value}\"\
+ for {parameter}"))
 
             types = real_type[2]
 
             if n_types != len(types):
-                raise (ConfigError(f"invalid argument\"{value}\" for {parameter}"))
+                raise (ConfigError(f"invalid argument\"{value}\"\
+ for {parameter}"))
 
             value = []
             for i, nested_real_type in enumerate(types):
@@ -80,7 +84,8 @@ class Config():
                 type_list.append(None)
                 type_list.append(nested_real_type)
                 type_list.append(False)
-                value.append(self.apply_types(parameter, type_list, new_value[i]))
+                value.append(self.apply_types(parameter,
+                                              type_list, new_value[i]))
             value = tuple(value)
 
         else:
@@ -102,12 +107,14 @@ class Config():
             new_value = value.split(separator)
 
             if len(new_value) != parameter_list[2]:
-                raise (ConfigError(f"invalid argument\"{value}\" for {parameter}"))
+                raise (ConfigError(f"invalid argument\"{value}\"\
+ for {parameter}"))
 
             types = parameter_list[3]
 
             if len(types) != parameter_list[2]:
-                raise (ConfigError(f"invalid argument\"{value}\" for {parameter}"))
+                raise (ConfigError(f"invalid argument\"{value}\"\
+ for {parameter}"))
 
             value = []
 
@@ -124,7 +131,8 @@ class Config():
             elif value.capitalize() == "False":
                 value = False
             else:
-                raise (ConfigError(f"invalid argument\"{value}\" for {parameter}"))
+                raise (ConfigError(f"invalid argument\"{value}\"\
+ for {parameter}"))
         else:
             value = parameter_list[1](value)
 
